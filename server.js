51e2852a3208fb16868ed438da075dfef891dde9
wahyudi124 <wahyudiprasatia3@gmail.com>
2019-05-16 11:25:11 +0700

    add new feature

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ var io = require('socket.io')(app);
 app.listen(5001);
 
 io.on('connection', function (socket) {
-    socket.on('join_room', room =>{
+      socket.on('join_room', room =>{
         socket.join(room)
       })
-  
+      
       socket.on('leave_room', room =>{
         socket.leave(room)
+      })
+
+
+      socket.on('control_data', data => {
+        io.in('room_control').emit('control_datas',data)
       })
   
       socket.on('rectifier_in_data', data =>{
@@ -19,5 +24,26 @@ io.on('connection', function (socket) {
       socket.on('ups_in_data', data =>{
         io.in('room_ups').emit('ups_datas',data)
       })
+
+      socket.on('pdu_in_data', data =>{
+        io.in('room_pdu').emit('pdu_datas',data)
+      })
+
+      socket.on('battery_in_data', data =>{
+        io.in('room_battery').emit('battery_datas',data)
+      })
+
+      socket.on('aircond_in_data', data =>{
+        io.in('room_aircond').emit('aircond_datas',data)
+      })
+
+      socket.on('sensor_in_data', data =>{
+        io.in('room_sensor').emit('sensor_datas',data)
+      })
+
+      socket.on('gpio_monitor_in_data', data =>{
+        io.in('room_gpio_monitor').emit('gpio_datas',data)
+      })
+
       console.log(socket.id)
 });
